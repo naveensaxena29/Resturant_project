0925e8c340635b7e8b81e00a5b6abfec363b0fcc
poonamsaxena123 <saxena2111poonam@gmail.com>
2026-05-22 21:30:09 +0530

    Fix Vercel deployment config

--- Resturant_Project/settings.py
+++ Resturant_Project/settings.py
@@ -175,6 +175,10 @@ LOGIN_REDIRECT_URL = '/'  # Redirect after successful login
 LOGOUT_REDIRECT_URL = '/login/'  # Redirect after logout
 
 
+DEBUG = False
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
+
+
 import os
 
 # Static files
